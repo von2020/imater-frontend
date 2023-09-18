bc5ee498b3718a0f2de899c28e35816c4787ff0a
yvonne orumwense <73657003+von2020@users.noreply.github.com>
2023-09-18 11:17:31 +0100

    update messages with phone number

--- baseUrl.js
+++ baseUrl.js
@@ -1,5 +1,8 @@
 const url = process.env.BaseUrl || "https://emmtect.vercel.app"; // this is for production and live
 
+// const url = process.env.BaseUrl || "http://localhost:5000"; // this is for production and live
+
+
 module.exports = {
   baseUrl: `${url}/api/v1/`,
 };
